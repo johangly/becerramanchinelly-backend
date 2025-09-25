8a0250c37db0627b32577e8e6ec9e15a645f1ec5
prodigy00 <calitoalejandro184@gmail.com>
2025-09-25 17:01:38 -0400

    hola

--- routes/paymentStripe.routes.js
+++ routes/paymentStripe.routes.js
@@ -1,6 +1,7 @@
 import db from "../database/index.js";
 import express from "express";
 import Stripe from "stripe"; // Cambiado el nombre del import
+import { createNotification } from "../utils/notificationHelper.js";
 
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // Instancia de Stripe
@@ -35,8 +36,8 @@ router.post("/create-checkout-session", async (req, res) => {
 				},
 			],
 			mode: "payment",
-			success_url: `${url}?success=true?appointmentId=${appointmentId}`,
-			cancel_url: `${url}?canceled=true`,
+			success_url: `${url}success?appointmentId=${appointmentId}&session_id={CHECKOUT_SESSION_ID}`,
+			cancel_url: `${url}canceled`,
 		});
 		console.log(session);
 		res.status(200).json({
@@ -53,4 +54,128 @@ router.post("/create-checkout-session", async (req, res) => {
 		});
 	}
 });
+router.get("/verify-session", async (req, res) => {
+	const { session_id } = req.query; // Obtener el parámetro de consulta session_id
+
+	try {
+		if (!session_id) {
+			return res.status(400).json({
+				status: "error",
+				message: "Missing required query parameter: session_id",
+			});
+		}
+
+		const session = await stripe.checkout.sessions.retrieve(session_id); // Recuperar la sesión de Stripe
+		console.log('session:', session);
+		res.status(200).json({
+			status: "success",
+			session,
+		});
+	} catch (error) {
+		console.error("Error verifying checkout session:", error);
+		res.status(500).json({
+			status: "error",
+			message: "Error verifying checkout session",
+			error: error.message,
+		});
+	}
+});
+router.put('/updateStatus',async(req,res)=>{
+		const transaction = await db.sequelize.transaction();
+
+	const {appointmentId,user_id,amount,client_name,paymentId} = req.body;
+	console.log(appointmentId,user_id,amount,client_name,paymentId)
+		const user = await db.User.findAll({
+				where: { cleark_id: user_id },
+			});
+			if (user.length === 0) {
+				return res.status(404).json({
+					status: "error",
+					message: "User not found",
+				});
+			}
+			const transactionDate = new Date();
+
+	try {
+		if(!appointmentId){
+			return res.status(400).json({
+				status: "error",
+				message: "Missing required fields: appointmentId"
+			});
+		}
+		const paymentAppointment =
+				await db.PaymentsAppointments.create(
+					{
+						paymentMethodId: 3,
+						status: "completado",
+						amount,
+						reference: paymentId,
+						client_name,
+						client_email: user[0].email,
+						user_id: user[0].id,
+						is_approved: null,
+						currency: "USD",
+						appointment_id: appointmentId
+							? parseInt(appointmentId)
+							: null,
+						is_approved: true,
+						transactionDate: transactionDate
+							? new Date(transactionDate)
+							: new Date(),
+						createdAt: new Date(),
+						updatedAt: new Date(),
+					},
+					{ transaction }
+				);
+
+		const appointment = await db.Appointment.findByPk(appointmentId);
+		if(!paymentAppointment){
+			return res.status(404).json({
+				status: "error",
+				message: "payment not found"
+			});
+		}
+		if (!appointment) {
+			return res.status(404).json({
+				status: "error",
+				message: "Appointment not found",
+			});
+		}
+		const updateAppointment=await appointment.update(
+			{
+				status: "reservado",
+			},
+			{
+				where: { id: appointmentId },
+				transaction,
+			});
+			await transaction.commit();
+		const notification = await createNotification({
+						userId: user[0].id,
+						title: `Your payment is ${paymentAppointment.status}.`,
+						message: `Your payment has been updated to ${paymentAppointment.status}.`,
+						type:
+							paymentAppointment === "completado"
+								? "success"
+								: paymentAppointment.status === "fallido"
+								? "error"
+								: "other",
+						payment_id: paymentAppointment.id,
+					}	);
+		res.status(200).json({
+			status: "success",
+			paymentAppointment,
+			updateAppointment
+		});
+
+	} catch (error) {
+		console.error("Error updating appointment status:", error);
+		res.status(500).json({
+			status: "error",
+			message: "Error updating appointment status",
+			error: error.message,
+		});
+	}
+});
+
 export default router;
