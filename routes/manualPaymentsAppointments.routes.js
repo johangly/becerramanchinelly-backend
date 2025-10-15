9c3da3512a6aae8cdbf69c99d0b962bdf90c1e38
Johangly <johanglyfabiansucre@gmail.com>
2025-10-15 12:05:49 -0400

    reordenando endpoint de proceso de creacion de external payments

--- routes/manualPaymentsAppointments.routes.js
+++ routes/manualPaymentsAppointments.routes.js
@@ -2,8 +2,8 @@ import express from "express";
 import db from "../database/index.js";
 import { uploadArray } from "../utils/manageFiles.js";
 import { createNotification } from "../utils/notificationHelper.js";
-import { da } from "zod/locales";
 import { TZDate } from "@date-fns/tz";
+import { logger } from "../utils/logger.js";
 
 const router = express.Router();
 
@@ -13,6 +13,8 @@ const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 const ZONE = process.env.ZONE_TIME;
 // Create a new payment appointment
 router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
+	logger.info("|||||||||||||||||||||||||||||||||||||||||||||||");
+	logger.info("New manual payment appointment");
 	const transaction = await db.sequelize.transaction();
 	try {
 		const formData = req.body;
@@ -29,7 +31,7 @@ router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
 			appointment_id,
 			transactionDate,
 		} = formData;
-
+		logger.info(`formData ${JOSIN.stringify(formData, null, 2)}`);
 		const user = await db.User.findAll({
 			where: { cleark_id: user_id },
 		});
@@ -39,13 +41,15 @@ router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
 		const PaymentsMethods = await db.PaymentsMethods.findAll({
 						where: { name: "Pago Externo" },
 					});
-		if(appointment[0].status === 'reservado'){
+		if (appointment[0].status === 'reservado') {
+			logger.info("Appointment is already booked");
 			return res.status(400).json({
 				status: "error",
 				message: "Appointment is already booked",
 			});
 		}
 		if (user.length === 0) {
+			logger.info("User not found");
 			return res.status(404).json({
 				status: "error",
 				message: "User not found",
@@ -78,7 +82,7 @@ router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
 				},
 				{ transaction }
 			);
-
+		logger.info(`paymentAppointment ${JSON.stringify(paymentAppointment, null, 2)}`);
 		// guarda la informacion de la imagen y su ruta
 		const createImagePayment = await db.PaymentImages.create(
 			{
@@ -99,6 +103,8 @@ router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
 			{ transaction }
 		);
 
+		logger.info(`createImagePayment ${JSON.stringify(createImagePayment, null, 2)}`);
+
 		const appointmentToUpdate = await db.Appointment.update(
 			{ status: "reservado" },
 			{
@@ -112,42 +118,60 @@ router.post("/", uploadArray("paymentImage", 1), async (req, res) => {
 			transaction
 		});
 
-		if(!adminUser){
+		if (!adminUser) {
+			logger.info("Admin user not found");
 			return res.status(404).json({
 				status: "error",
 				message: "Admin user not found",
 			});
 		}
-		await transaction.commit();
 
-		if (adminUser) {
-			// Crear notificación para el administrador
-			await db.Notification.create({
-				title: 'Nuevo pago pendiente por aprobar',
-				body: `Se ha recibido un pago por el monto de ${paymentAppointment.amount}$${user.name ? ` por el usuario ${user.name}` : ''}`,
-				type: 'success',
-				modalBody: `Se ha recibido un pago por el monto de ${paymentAppointment.amount}$${user.name ? ` por el usuario ${user.name}` : ''} para la fecha ${new TZDate(appointmentToUpdate.day, ZONE).internal} que inicia a las ${new TZDate(appointmentToUpdate.start_time, ZONE).internal} y termina a las ${new TZDate(appointmentToUpdate.end_time, ZONE).internal}
-				| Nombre del cliente: ${client_name}
-				| Telefono del cliente: ${client_phone}
-				| Correo electrónico: ${client_email}
-				| Fecha de transacción: ${new TZDate(transactionDate, ZONE).internal}
-				| Referencia: ${paymentAppointment.reference}
-				| Monto del pago: ${paymentAppointment.amount}
-				| Notas: ${notes}
-				| Metodo de pago: Pago Externo
-				`,
-				user_id: adminUser.id,
-				payment_id: paymentAppointment.id,
-				created_at: new TZDate(new Date(), ZONE).internal,
-				updated_at: new TZDate(new Date(), ZONE).internal
-			});
-		}
+		logger.info(`appointmentToUpdate ${JSON.stringify(appointmentToUpdate, null, 2)}`);
 
-
-		res.status(201).json({
-			status: "success",
-			data: paymentAppointment,
+		// Crear notificación para el administrador
+		const creatingNotification = await db.Notification.create({
+			title: 'Nuevo pago pendiente por aprobar',
+			body: `Se ha recibido un pago por el monto de ${paymentAppointment.amount}$${user.name ? ` por el usuario ${user.name}` : ''}`,
+			type: 'success',
+			modalBody: `Se ha recibido un pago por el monto de ${paymentAppointment.amount}$${user.name ? ` por el usuario ${user.name}` : ''} para la fecha ${new TZDate(appointmentToUpdate.day, ZONE).internal} que inicia a las ${new TZDate(appointmentToUpdate.start_time, ZONE).internal} y termina a las ${new TZDate(appointmentToUpdate.end_time, ZONE).internal}
+			| Nombre del cliente: ${client_name}
+			| Telefono del cliente: ${client_phone}
+			| Correo electrónico: ${client_email}
+			| Fecha de transacción: ${new TZDate(transactionDate, ZONE).internal}
+			| Referencia: ${paymentAppointment.reference}
+			| Monto del pago: ${paymentAppointment.amount}
+			| Notas: ${notes}
+			| Metodo de pago: Pago Externo
+			`,
+			user_id: adminUser.id,
+			payment_id: paymentAppointment.id,
+			created_at: new TZDate(new Date(), ZONE).internal,
+			updated_at: new TZDate(new Date(), ZONE).internal
 		});
+		logger.info(`creatingNotification ${JSON.stringify(creatingNotification, null, 2)}`);
+
+		if (creatingNotification) {
+			logger.info("Notification created successfully");
+
+			await transaction.commit();
+
+			logger.info("Proceso completado!");
+			logger.info("|||||||||||||||||||||||||||||||||||||||||||||||");
+
+			res.status(201).json({
+				status: "success",
+				data: paymentAppointment,
+			});
+
+		} else {
+			logger.error("Error creating notification");
+
+			return res.status(500).json({
+				status: "error",
+				message: "Error creating notification",
+			});
+
+		}
 	} catch (error) {
 		await transaction.rollback();
 		console.error("Error creating payment appointment:", error);
